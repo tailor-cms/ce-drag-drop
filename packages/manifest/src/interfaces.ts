826aff989914592a4eaaf98b158edabce0f46a7d
rkusan00 <renato.kusanovic@gmail.com>
2025-02-04 16:05:45 +0100

    Revert to old structutre with question prop

--- packages/manifest/src/interfaces.ts
+++ packages/manifest/src/interfaces.ts
@@ -1,6 +1,7 @@
 export interface ElementData {
   isGradable?: boolean;
   embeds: Record<string, any>;
+  question: string[];
   groups: Record<string, string>;
   answers: Record<string, string>;
   correct: Record<string, string[]>;

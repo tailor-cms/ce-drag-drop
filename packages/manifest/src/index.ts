f027bfed0af1bf44348b8c4c222606bf57b2d973
rkusan00 <renato.kusanovic@gmail.com>
2025-02-04 13:51:11 +0100

    Update icon

--- packages/manifest/src/index.ts
+++ packages/manifest/src/index.ts
@@ -39,7 +39,7 @@ export const version = '1.0';
 // UI configuration for Tailor CMS
 const ui = {
   // Display icon, https://pictogrammers.com/library/mdi/
-  icon: 'mdi-format-columns',
+  icon: 'mdi-cursor-move',
   // Does element support only full width or can be used within layouts
   // (e.g. 50/50 layout)
   forceFullWidth: true,

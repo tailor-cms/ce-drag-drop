3bd4781a09ba475bb453ab24d41c6641e884be95
rkusan00 <renato.kusanovic@gmail.com>
2026-04-22 17:43:03 +0200

    Cleanup

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -54,8 +54,8 @@ export const onUserInteraction: OnUserInteractionHook<Element> = (
 export const hookMap: HookMap<Element> = new Map(
   Object.entries({
     afterLoaded,
-    onUserInteraction,
     beforeDisplay,
+    onUserInteraction,
   }),
 );
 
@@ -64,8 +64,8 @@ const serverModule: ServerModule<Element> = {
   initState,
   hookMap,
   afterLoaded,
-  onUserInteraction,
   beforeDisplay,
+  onUserInteraction,
   mocks,
   ai,
 };
